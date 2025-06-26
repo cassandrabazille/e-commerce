a590e8ed63445382a82da633e2fa6dccc35daa98
Cassandra Bazille <bazillecassandra@gmail.com>
2025-06-26 10:53:32 +0200

    adding routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+<<<<<<< Updated upstream
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
@@ -12,3 +13,17 @@ Route::get('/', function () {
 
 Route::get('login', [AuthController::class, 'login']);
 Route::post('login', [AuthController::class, 'doLogin'])->name('auth.login');
+=======
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProduitController;
+use App\Http\Controllers\CategorieController;
+
+Route::resource('produit', ProduitController::class);
+Route::resource('categorie', CategorieController::class);
+
+
+// Route::patch('produit/{id}/valider', [ProduitController::class, 'valider'])->name('produit.valider');
+// Route::get('produit/{id}/gestionProduit', [ProduitController::class, 'gestionProduit'])->name('produit.gestionProduit');
+// Route::get('categorie/gestionCategorie', [CategorieController::class, 'index'])->name('categorie.gestionCategorie');
+>>>>>>> Stashed changes
